d8f63e8e94ae9d8ccaf93ee50e245f0a76f6db21
Rachel Muzika <rpolatsc@gmail.com>
2023-01-23 14:01:15 -0500

    Specials style

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,33 +1,51 @@
 import greekSalad from './images/greekSalad.jpg';
 import bruchetta from './images/bruchetta.png';
 import lemonDessert from './images/lemonDessert.jpg';
+import { Link } from 'react-router-dom';
+
+const linkStyle = {
+    color: "black",
+}
 
 function Specials() {
     return(
         <>
             <div className='specials'>
-                <div className='flex'>
+                <div className='specials-top'>
                     <h2>This week's specials!</h2>
-                    <button>Online Menu</button>
+                    <button>
+                        <Link to="/online-order" style={linkStyle}>Online Menu</Link>
+                    </button>
                 </div>
-                <div className='flex'>
-                    <div>
-                        <img src={greekSalad} width="200rem" alt="greek salad" />
-                        <p>Greek salad</p>
-                        <p>$12.99</p>
-                        <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                <div className='specials-cards'>
+                    <div className='specials-card'>
+                        <img src={greekSalad} alt="greek salad" />
+                        <div className='specials-description'>
+                            <p className='dish-name'>Greek salad</p>
+                            <p className='dish-price'>$12.99</p>
+                            <p className='dish-description'>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                            <p className='dish-order'>Order a delivery</p>
+                        </div>
                     </div>
-                    <div>
-                        <img src={bruchetta} width="200rem" alt="bruchetta" />
-                        <p>Bruchetta</p>
-                        <p>$5.99</p>
-                        <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
+                    <div className='specials-card'>
+                        <img src={bruchetta} alt="bruchetta" />
+                        <div className='specials-description'>
+                            <p className='dish-name'>Bruchetta</p>
+                            <p className='dish-price'>$5.99</p>
+                            <p className='dish-description'>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
+                            <br/>
+                            <p className='dish-order'>Order a delivery</p>
+                        </div>
                     </div>
-                    <div>
-                        <img src={lemonDessert} width="200rem" alt="lemon dessert" />
-                        <p>Lemon dessert</p>
-                        <p>$5.00</p>
-                        <p>This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+                    <div className='specials-card'>
+                        <img src={lemonDessert} alt="lemon dessert" />
+                        <div className='specials-description'>
+                            <p className='dish-name'>Lemon dessert</p>
+                            <p className='dish-price'>$5.00</p>
+                            <p className='dish-description'>This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+                            <br/>
+                            <p className='dish-order'>Order a delivery</p>
+                        </div>
                     </div>
                 </div>
             </div>

775e727ca331807ace11539bc3b9c60d8ac2c879
Rachel Muzika <rpolatsc@gmail.com>
2023-01-23 15:39:16 -0500

    Chicago styles

--- src/components/Chicago.js
+++ src/components/Chicago.js
@@ -1,15 +1,20 @@
 import imgMain from './images/MarioandAdrianA.jpg';
+import imgCookers from './images/MarioandAdrianb.jpg';
 
 function Chicago() {
     return(
         <>
-            <div className='flex about'>
-                <div>
+            <div className='chicago'>
+                <div className='chicago-column'>
                     <h1>Little Lemon</h1>
                     <h2>Chicago</h2>
-                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
                 </div>
-                <img src={imgMain} width="50%" alt="restaurant cheffs" />
+                <div className='chicago-img'>
+                    <img src={imgCookers} className="img2" alt="restaurant cheffs" />
+                    <img src={imgMain} className="img1" alt="restaurant cheffs" />
+                </div>
             </div>
         </>
     )
